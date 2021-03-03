ff50b2ec5aaea4b5624fca83ddebcde63d27251d
Elizabeth <elrestre22@gmail.com>
2021-03-03 10:05:19 +0100

    Create Serializers

---
+++ api_movies/serializers.py
@@ -0,0 +1,13 @@
+from .models import Character, Movie
+from rest_framework import serializers
+
+class CharacterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        fields = '__all__'
+
+class MovieSerializer(serializers.ModelSerializer):
+    characters = CharacterSerializer(read_only=True, many=True)
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'episode_id', 'resume', 'director', 'producer', 'release_date', 'characters']
