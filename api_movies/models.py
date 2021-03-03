a0fc790a7845b82184ebd0e75951bb5db50b7be4
Elizabeth <elrestre22@gmail.com>
2021-03-03 09:58:55 +0100

    Create Movie Model

--- api_movies/models.py
+++ api_movies/models.py
@@ -11,3 +11,15 @@ class Character(models.Model):
     def __str__(self):
         return str(self.id)
 
+class Movie(modesl.Model):
+    title = models.TextField()
+    episode_id = models.IntegerField()
+    resume = models.TextField(blank=True)
+    director = models.CharField(max_length=200, blank=True)
+    producer = models.CharField(max_length=200, blank=True)
+    release_date = models.DateField()
+    url = models.CharField(max_length=100, blank=True)
+    characters = models.ManyToManyField(Character, related_name="Characters", blank=True)
+
+    def __str__(self):
+        return self.title
