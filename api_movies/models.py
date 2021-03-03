3fac53d48362ad0e560096e7c95d35bf9d80aa1a
Elizabeth <elrestre22@gmail.com>
2021-03-03 09:54:57 +0100

    Create Character Model

--- api_movies/models.py
+++ api_movies/models.py
@@ -1,3 +1,13 @@
+from django.contrib import admin
 from django.db import models
 
-# Create your models here.
+class Character(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    birth_year = models.CharField(max_length=50, blank=True)
+    gender = models.CharField(max_length=20, blank=True)
+    imagen = models.CharField(max_length=100, blank=True)
+    url = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return str(self.id)
+
